c6c17f8266eb48cd373dc573b1b29fbb7dbe9e64
daejun4582 <qkseowns9206@gmail.com>
2023-03-09 04:00:44 +0900

    quick sort 추가

--- implementation/function_pointer.cpp
+++ implementation/function_pointer.cpp
@@ -6,7 +6,7 @@ using namespace std;
 
 
 int comp_int(const void *a,const void *b){
-    return (*((int*)b)-*((int*)a));
+    return (*((int*)a)-*((int*)b));
 }
 
 int comp_char(const void *a,const void *b){
@@ -15,10 +15,10 @@ int comp_char(const void *a,const void *b){
 
 
 int main(void){
-    // int values[] = { 4, 6, 3, 2, 5, 7, 9, 8};
-    char values[] = { 'n', 'i', 'b', 'c'};
+    int values[] = { 4, 6, 3, 2, 5, 7, 9, 8};
+    // char values[] = { 'n', 'i', 'b', 'c'};
     int n = sizeof(values)/sizeof(values[0]);
-    qsort(values,n,sizeof(values[0]),comp_char);
+    qsort(values,n,sizeof(values[0]),comp_int);
 
     for(int i = 0; i < n; i++){
         cout<<values[i]<<" "; 
--- implementation/sortings.cpp
+++ implementation/sortings.cpp
@@ -5,13 +5,20 @@ using namespace std;
 void bubble_sort(int *p,int n);
 void insertion_sort(int *p,int n);
 void selection_sort(int *p,int n);
+void quick_sort(int p[],int start, int end,int size);
+
+void swap(int *a, int *b);
+void print_process(int p[],int size);
 
 int main(void){
     int values[] = { 4, 6, 3, 2, 5, 7, 9, 8};
     int n = sizeof(values)/sizeof(values[0]);
+     
 
     void (*sortings[])(int *,int) = {bubble_sort,insertion_sort,selection_sort};
-    sortings[2](values,n);
+    
+    // sortings[3](values,n);
+    quick_sort(values,0,n-1,n);
 
 
     for(int i = 0; i<n; i++){
@@ -25,15 +32,10 @@ void bubble_sort(int *p,int n){
     for (int i = 0; i<n; i++){
         for(int j = 0; j<i; j++){
             if(p[j]>p[j+1]){
-                temp = p[j];
-                p[j] = p[j+1];
-                p[j+1] = temp;
+                swap(p[j],p[j+1]);
             }
         }
-        for(int k = 0; k<n; k++){
-            cout<<p[k]<<" ";
-        }
-        printf("\n");
+        print_process(p,n);
     }
 }
 
@@ -49,13 +51,8 @@ void insertion_sort(int *p,int n){
                 min = j;
             }
         }
-        temp = p[min];
-        p[min] = p[i];
-        p[i] = temp;
-        for(int k = 0; k<n; k++){
-            cout<<p[k]<<" ";
-        }
-        printf("\n");
+        swap(p[min],p[i]);
+        print_process(p,n);
     }
     
 
@@ -66,14 +63,46 @@ void selection_sort(int *p,int n){
     for(int i = 0; i<n; i++){
         for(int j = i+1; j<n; j++){
             if(p[i]>p[j]){
-                temp = p[i];
-                p[i] = p[j];
-                p[j] = temp;
+                swap(p[i],p[j]);
             }
-            for(int k = 0; k<n; k++){
-                cout<<p[k]<<" ";
-            }
-            printf("\n");
+            print_process(p,n);
         }
     }
+}
+
+
+
+void quick_sort(int p[],int start, int end,int size){
+    printf("start : %d end: %d\n",start,end);
+    int lo = start+1,hi = end,pivot = start;
+    if(start >= end) return;
+    
+    while(lo<= hi){
+        
+        while(lo <= end && p[pivot] >= p[lo]) lo++;
+        while(hi > start && p[pivot] <= p[hi]) hi--;
+
+
+        if(lo>hi) swap(p[hi],p[pivot]);
+        else swap(p[hi],p[lo]);
+    }
+    print_process(p,size);
+    quick_sort(p,start,hi-1,size);
+    quick_sort(p,hi+1,end,size);
+
+}
+
+
+void swap(int *a, int *b){
+    int temp;
+    temp = *a;
+    *a = *b;
+    *b = temp;
+}
+
+void print_process(int p[],int size){
+    for(int i = 0; i<size; i++){
+        cout<<p[i]<<" ";
+    }
+    cout<<"\n";
 }
